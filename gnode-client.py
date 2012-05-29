2d9467d5eb18ae1ccf0a02a9da40e47d6d16d641
Michael <michael@michael-ThinkPad-T400s.(none)>
2012-05-29 20:31:56 +0200

    commit first script (test)

---
+++ gnode-client.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+
+#http://stackoverflow.com/questions/712791/json-and-simplejson-module-differences-in-python for details
+try: import simplejson as json
+	except ImportError: import json
