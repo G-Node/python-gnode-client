84e79e2da66566859ad1f754d20dd5f50c613c53
Michael Pereira <michael.fsp@gmail.com>
2012-08-09 11:04:02 +0100

    added https support

--- session.py
+++ session.py
@@ -1,30 +1,75 @@
-from utils import load_profile
+#!/usr/bin/env python
+import requests
+
+import simplejson as json
+
+from utils import load_profile, authenticate
 
 
-def init( *args, **kwargs ):
+def init(config_file='default.json', *args, **kwargs ):
     """ some easy to remember function to start g-node session """
-    return Session( *args, **kwargs )
+    url, username, password = load_profile(config_file)
+    return Session(url, username, password, *args, **kwargs )
 
 
-class Session( object ):
+class Session(object):
     """ Object to handle connection and client-server data transfer """
 
-    def __init__(self, *args, **kwargs ):
-        self.cookie_jar = utils.authenticate( load_profile() )
+    def __init__(self, url, username, password, *args, **kwargs ):
+        self.url = url
+        self.username = username
+        self.password = password
+        self.cookie_jar = authenticate(self.url, self.username,
+            self.password)
+        self.auth_cookie = self.cookie_jar['sessionid']
         # TODO of course make it more flexible
 
 
-    def get(self, obj_type, obj_id=None, *kwargs):
-        """ get one or several objects from the server """
+    def get(self, obj_type, obj_id=None, verbosity=None):
+        """Get one or several objects from the server of a given object type.
 
-        # send appropriate GET request to get objects from the server
+        Args:
+            obj_type: block, segment, event, eventarray, epoch, epocharray,
+                unit, spiketrain, analogsignal, analogsignalarray,
+                irsaanalogsignal, spike, recordingchannelgroup or
+                recordingchannel
+
+            obj_id: the id of the objects to retrieve as an integer or a list
+
+            verbosity: controls the amount of information about the received objects
+            'link' -- just permalink
+            'info' -- object with local attributes
+            'beard' -- object with local attributes AND foreign keys resolved
+            'data' -- data-arrays or any high-volume data associated
+            'full' -- everything mentioned above
+        """
+        #accept obj_id as a single element or list (or tuple)
+        if type(obj_id) is not list and type(obj_id) is not tuple:
+            obj_id = [obj_id]
+
+        objects = []
+
+        if not verbosity:
+        #TODO check that I am reading JSON from the right object
+            for obj in obj_id:
+                resp = requests.get(self.url+str(obejct_type)+'/'+str(
+                    object_id)+'/', cookies=self.auth_cookie)
+                json_obj = resp.json
+                objects.append(json.loads(json_obj))
+        else:
+            for obj in obj_id:
+                resp = requests.get(self.url+str(obejct_type)+'/'+str(
+                    object_id)+'/'+'?q='+str(verbosity)+'/',
+                    cookies=self.auth_cookie)
+                json_obj = resp.json
+                objects.append(json.loads(json_obj))
 
         # deserialize received JSON object
-        pass
+        return objects
 
 
     def create(self, obj, *kwargs):
-        """ saves new object to the server """
+        """Saves new object to the server """
 
         # serialize to JSON
 
