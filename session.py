b0f0bce0d967562fb03b670e2a6fa9919603d5b7
andrey <sobolev@bio.lmu.de>
2013-03-28 23:54:13 +0100

    cd function draft done

--- session.py
+++ session.py
@@ -56,7 +56,6 @@ class Browser(object):
 
     ls_filt = {} # display filters
     location = '' # current location, like 'metadata/section/293847/'
-    default_view = 'section'
 
     def ls(self, filt=None):
         """ cmd-type ls function """
@@ -64,22 +63,47 @@ class Browser(object):
         params = dict( self.ls_filt )
 
         if self.location:
-            app, cls, lid = self._parse_location()
+            app, cls, lid = self._parse_location( self.location )
 
-            for child in self.app_definitions[ curr_type ]:
-                params[ curr_type + '__id' ] = lid
+            for child in self.app_definitions[ cls ]['children']:
+
+                parent_name = cls
+                # FIXME dirty fix!! stupid data model inconsistency
+                if (cls == 'section' and child == 'section') or \
+                    (cls == 'property' and child == 'value'):
+                    parent_name = 'parent_' + parent_name
+
+                params[ parent_name + '__id' ] = lid
                 objs = self.get(child, params=params, cascade=False)
+
                 out = self._render( objs, out )
-                params.pop[ curr_type + '__id' ]
+                params.pop( parent_name + '__id' )
         else:
-            objs = self.get(self.default_view, params=params, cascade=False)
+            params['parent_section__isnull'] = 1
+            objs = self.get('section', params=params, cascade=False)
             out = self._render( objs, out )
 
         print out
 
     def cd(self, location=''):
         """ changes the current location within the data structure """
-        pass
+        if location == '':
+            self.location = ''
+            print 'back to root'
+
+        else:
+            # 1. compile url
+            url = str( location )
+            if is_permalink( location ):
+                url = url.replace(self.url, '')
+            app, cls, lid = self._parse_location( url )
+
+            # 2. get the object at the location - raises error if not accessible
+            obj = self.get(cls, id=lid, cascade=False)
+
+            self.location = url
+            print "entered %s" % url
+
 
     def _render(self, objs, out):
         """ renders a list of objects for a *nice* output """
@@ -90,10 +114,10 @@ class Browser(object):
             out += obj.__repr__()[ : max_line_out ] + '\n'
         return out
 
-    def _parse_location(self):
+    def _parse_location(self, location):
         """ extracts app name and object type from the current location, e.g.
         'metadata' and 'section' from 'metadata/section/293847/' """
-        l = str( self.location )
+        l = str( location )
 
         if l.startswith('/'):
             l = l[ 1 : ]
