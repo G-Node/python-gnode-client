dc9ee8c87dde5d84b16833fe240ae6f3ce4140c5
andrey <sobolev@bio.lmu.de>
2013-04-12 14:42:42 +0200

    added analysis part of the script to the Thomas use case

--- use_case_thomas.py
+++ use_case_thomas.py
@@ -189,6 +189,7 @@ if not data_created:
             data = convert_to_timeseries( l ) * pq.mV
 
             kwargs = {}
+            kwargs['name'] = 'Trial %d Ch %d' % (i % 176, i / 176)
             kwargs['t_start'] = 0 * pq.ms
 
             sr = 500.0 #float( hardware.properties['LfpSamplingRate'].value.data )
@@ -230,4 +231,138 @@ for i, s in enumerate( b.segments ):
 
     g.annotate([s], [color, orient, cond])
 
+
+# 4. use case
+#-------------------------------------------------------------------------------
+
+if 0: # not implemented yet
+
+    # STEP 1 - 2
+    """ First, the spike times should be scaled because sampling rate 
+    was 500Hz, i.e., the values have to be multiplied by 2 to represent time in 
+    ms. Furthermore, trials are time-aligned to stimulus onset or saccade end, 
+    respectively, which should be t=0. This means that 300ms should be 
+    subtracted from spike times, and the first value of the lfp traces 
+    corresponds to t=-300 ms."""
+
+    # option 1
+    for segment in b.segments():
+        for st in segment.spiketrains:
+            st.times *= 2.0
+            st.times -= 300.0
+            st.save() # sends data to the server
+
+    # option 2
+    for st in gnode.select("spiketrain", segment__in = b.segments()):
+        st.times *= 2.0
+        st.times -= 300.0
+        st.save() # sends data to the server
+
+
+    # STEP 3
+    """ For each LFP channel, calculate the averages of the LFP traces over all 
+    trials for each unique experimental condition (2 behav cond, 4x4 stimuli). 
+    Save the averages because they will be used often. Plot the LFP responses 
+    together with the mean for a given LFP channel, color, and orientation."""
+
+    averages = b.add_segment("Averages") # a fake segment to store averages
+    for cond in p_cond.values():
+        for color in p_color.values():
+            for angle in p_orient.values():
+                for channel in b.recordingchannels:
+                    """ notice here, we filter signals which do not have DIRECT 
+                    experimental conditions assigned (color etc.) but the following
+                    should still work through implicit filtering (through segment 
+                    relation, which holds these attributes)."""
+
+                    # data from relevant block, channel, condition, color and angle
+                    # option 1
+                    signals = gnode.select("analogsignal", block = b) # query should not be evaluated here!!
+                    signals = signals.filter(recordingchannel=channel) # neither here!!
+                    # nor here!!
+                    signals = signals.filter(metadata = (p_cond, cond))
+                    signals = signals.filter(metadata = (p_color, color))
+                    signals = signals.filter(metadata = (p_orient, angle))
+
+                    # OR option 2
+                    criteria = {
+                        "block": b,
+                        "recordingchannel": channel,
+                        "metadata1": (p_cond, cond),
+                        "metadata2": (p_color, color),
+                        "metadata3": (p_orient, angle)
+                    }
+                    signals = gnode.select("analogsignal", **criteria)
+
+                    # query should be evaluated only at next line
+                    averaged_signal = signals[0].clone() # this is interesting, ah?
+                    averaged_signal.signal = to_array(signals).mean(axis=0) # ?!
+                     # should it be already numpy arrays?
+                    averaged_signal.segment = averages
+                    averaged_signal.save()
+
+    # STEP 4
+    """ Plot the LFP responses together with the mean for a given LFP channel, 
+    color, and orientation. Here let's take condition = FIX, color = red, 
+    orientation = 45 grad, channel = 5."""
+
+    import matplotlib.pyplot as plt
+    criteria = {
+        "recordingchannel": gnode.select("recordingchannel", block=b, index=5)
+        "metadata1": (p_cond, "fixation"),
+        "metadata2": (p_color, "red"),
+        "metadata3": (p_orient, "45")
+    }
+    selected = gnode.select("analogsignal", **criteria)
+
+    # within selected should be all signals, including the average
+    plt.plot(selected)
+
+    # STEP 5
+    """ Determine the peak values of the average LFP responses and find the LFP
+    channel with the largest difference between largest and smallest peak values
+    across stimuli."""
+
+    deltas = []
+    for channel in b.recordingchannels:
+        selected = gnode.select("analogsignal", segment = averages, recordingchannel = channel)
+        ar = to_array(selected) # should it be already numpy arrays?
+        delta = abs(ar.max() - ar.min())
+        if delta > max(deltas): res_ch = channel # the winning channel
+        deltas.append(delta)
+
+    # STEP 6
+    """ Make a dataset that contains all available data (lfp and spikes) from
+    the corresponding electrode and share it with user XY."""
+
+    """ this should automatically share all related objects, like analogsignals,
+    spiketrains, units, block etc. """
+    res_ch.share_with(name="XY")
+
+    # STEP 7 - 8
+    """ For each SUA channel, calculate the mean spike rate in the 100ms time 
+    interval starting 40ms after stimulus onset/saccade end for each stimulus in
+    a given behavioral condition. The resulting two sets of 16 response values 
+    (4x4 response matrices) represent the stimulus selectivities that 
+    characterize the unit. Save the values because they will be used often."""
+
+    rs.add_section("Analysis", template="analysis") # switch the logging on?
+
+    # TODO
+
+    #---------------------------------------------------------------------------
+
+    """ Opened questions:
+    - how do blocks (segments etc.) relate to their metadata?!
+    - what to do with duplicates?
+    - where to assign experimental conditions - to segment or to analogsignal? or both?
+
+
+        Important functions:
+    - <object_type>.filter(conditions, names etc.) or <queryset>.filter
+    - making an ndim array from queryset..
+
+    """
+
+    #---------------------------------------------------------------------------
 
