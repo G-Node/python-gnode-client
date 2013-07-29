fdb76f271e18d5dff5cabd44111e7c0700dd1d4f
andrey <sobolev@bio.lmu.de>
2013-07-29 17:01:24 +0200

    update_object func written, not tested

--- backend/cache.py
+++ backend/cache.py
@@ -30,12 +30,49 @@ class Cache( object ):
         if meta.load_cached_data:
             self.load_cached_data()
 
-    def add_object(self, obj):
-        """ adds object to cache """
-        location = self._meta.clean_location( obj._gnode['location'] )
-        guid = obj._gnode['fields']['guid']
+    def update_object(self, obj):
+        """ adds/updates object in the cache (non-cascade) """
+        model_name = get_type_by_obj(obj)
+        app_name = self._meta.app_prefix_dict[ model_name ]
+        json_obj = {}
+        data_refs = {}
+
+        # 1. detect / generate IDs
+        if hasattr(obj, '_gnode'): # synced object
+            guid = obj._gnode['fields']['guid']
+            location = self._meta.clean_location( obj._gnode['location'] )
+
+        else:
+            lid = generate_temp_id()
+            guid = generate_temp_guid()
+            location = "/%s/%s/%s/" % (app_name, model_name, lid)
+
+        # 2. save data if new or was changed
+        attrs_to_save = self.__detect_changed_data_fields(obj)
+        for attr in attrs_to_save:
+            fname = self._meta.app_definitions[model_name]['data_fields'][attr][2]
+            if fname == 'self':
+                arr = obj # some NEO objects like signal inherit array
+            else:
+                arr = getattr(obj, fname)
+
+            if not type(arr) == type(None): # because of NEO __eq__
+                units = Serializer.parse_units(arr)
+                datapath = self._cache.save_data(arr)
+                data_refs[ attr ] = {'data': datalink, 'units': units}
+
+            else:
+                data_refs[ attr ] = None
+
+        # 3. update cached values
         self.objs_map[ location ] = guid
         self.objs[ guid ] = obj
+
+        json_obj['guid'] = guid
+        json_obj['location'] = location
+        json_obj['new_data_references'] = data_refs
+        return json_obj
+
 
     def clear_cache(self):
         """ removes all objects from the cache """
@@ -112,4 +149,51 @@ class Cache( object ):
 
         return datapath
 
+
+    def __detect_changed_data_fields(self, obj):
+        """ compares all current in-memory data fields (arrays) for a given 
+        object with cached (on-disk) versions of these data arrays and returns
+        names of the fields where arrays do NOT match """
+
+        attrs_to_sync = []
+        model_name = get_type_by_obj( obj )
+        data_fields = self._meta.app_definitions[model_name]['data_fields']
+        data_attrs = self._meta.get_array_attr_names( model_name )
+
+        if not hasattr(obj, '_gnode'):
+            return data_attrs # object is new, all data attrs must be saved
+
+        for attr in data_attrs:
+
+            data_value = obj._gnode['fields'][ attr ]['data']
+
+            if data_value:
+                data_info = self.get_data( data_value )
+
+                if not data_info == None:
+                    # get actual array
+                    getter = data_fields[attr][2]
+                    if getter == 'self':
+                        # some NEO objects like signal inherit array
+                        curr_arr = obj
+                    else:
+                        curr_arr = getattr(obj, getter)
+
+                    if len(curr_arr) < 2:
+                        # we treat array with < 2 values as when object was 
+                        # fetched without data for performance reasons. in this 
+                        # case we ignore this data attribute
+                        continue
+
+                    # compare cached (original) and current data
+                    if not np.array_equal(data_info['data'], curr_arr):
+                        attrs_to_sync.append( attr )
+
+                else: # no data!
+                    attrs_to_sync.append( attr )
+
+            else: # no real reference! treat as array was changed
+                attrs_to_sync.append( attr )
+
+        return attrs_to_sync
 
