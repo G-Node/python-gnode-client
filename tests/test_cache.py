66fea52b85b0575bab2ed800aa9e1cb70dd1ea0d
A. Stoewer <adrian.stoewer@rz.ifi.lmu.de>
2013-09-11 15:24:09 +0200

    Test: test for pull() with cascade.

--- tests/test_cache.py
+++ tests/test_cache.py
@@ -1,8 +1,15 @@
 import unittest
 
-from test_data import TestDataCollection
+import numpy as np
+import quantities as pq
 
+from neo import Block, Segment, EventArray, Event, EpochArray, Epoch, RecordingChannelGroup, \
+    RecordingChannel, Unit, SpikeTrain, Spike, AnalogSignalArray, AnalogSignal, \
+    IrregularlySampledSignal
 from gnode import session
+from gnode.rest_model import Models
+
+from test_data import TestDataCollection, r_times
 
 
 class TestCache(unittest.TestCase):
@@ -54,15 +61,62 @@ class TestCache(unittest.TestCase):
             msg = "Failed to pull data from locaction: %s" % location
             self.assertIsNotNone(result, msg)
 
-            result = self.session.cache.get_data(location)
+            result = self.session.cache.pull(location)
 
             msg = "Pulled data not cached"
             self.assertIsNotNone(result, msg)
 
             self.session.cache.clear_cache()
 
-    def test_02(self):
-        self.failUnlessEqual(2, 2)
+    def test_02_pull_cascade(self):
+        child_count = 4
+
+        # prepare test data
+        bl = Block("block_1")
+        for i in range(child_count):
+            seg = Segment(name="seg_%d" % i, index=i)
+            bl.segments.append(seg)
+
+            rcg = RecordingChannelGroup(name="gcg_%d" % i)
+            bl.recordingchannelgroups.append(rcg)
+
+            for j in range(child_count):
+                rc = RecordingChannel(index=j, name="rc_%d_%d" % (i, j))
+                rcg.recordingchannels.append(rc)
+
+                sig = AnalogSignal(np.random.rand(100) * pq.mV, sampling_period=0.1 * pq.s, name="sig_%d_%d" % (i, j))
+                rc.analogsignals.append(sig)
+                seg.analogsignals.append(sig)
+
+        # push data
+        self.session.push(bl, cascade=True, force_update=True)
+        self.session.cache.clear_cache()
+
+        msg = "Failed to push data: %s!" % str(bl)
+        self.assertTrue(hasattr(bl, "_gnode"), msg)
+
+        # pull data
+        location = bl._gnode["location"]
+        bl_pulled = self.session.pull(bl._gnode["location"])
+
+        msg = "Failed to pull data from location: %s!" % location
+        self.assertIsNotNone(bl_pulled, msg)
+
+        count = len(bl_pulled.segments)
+        msg = "Segment count is %d but %d was expected" % (count, child_count)
+        self.assertEqual(count, child_count, msg)
+
+        count = len(bl_pulled.recordingchannelgroups)
+        msg = "RecordingChannelGroup count is %d but %d was expected" % (count, child_count)
+        self.assertEqual(count, child_count, msg)
+
+        count = len(bl_pulled.recordingchannelgroups[0].recordingchannels)
+        msg = "RecordingChannel count is %d but %d was expected" % (count, child_count)
+        self.assertEqual(count, child_count, msg)
+
+        count = len(bl_pulled.segments[0].analogsignals)
+        msg = "AnalogSignal count is %d but %d was expected" % (count, child_count)
+        self.assertEqual(count, child_count, msg)
 
 
 if __name__ == "__main__":
