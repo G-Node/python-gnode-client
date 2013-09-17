ad0a68b60a4ad3855ae2c05e97b3d96dc471d627
A. Stoewer <adrian.stoewer@rz.ifi.lmu.de>
2013-09-17 17:24:07 +0200

    Model: definition for odml and some more for neo

--- gnodeclient/model/rest_model.py
+++ gnodeclient/model/rest_model.py
@@ -78,13 +78,55 @@ class RestResult(Model):
         return template % (self.model, self.location, name_str)
 
 
+class SectionModel(RestResult):
+    model = Field(type=str, default=Models.SECTION)
+    name = Field(type=str, optional=False)
+    description = Field(type=str)
+
+    parent = Field(is_parent=True, type_info=Models.SECTION)
+    properties = Field(is_child=True, type=list, type_info=Models.PROPERTY)
+
+Models._MODEL_MAP[Models.SECTION] = SectionModel
+
+
+class PropertyModel(RestResult):
+    model = Field(type=str, default=Models.PROPERTY)
+    name = Field(type=str, optional=False)
+
+    parent = Field(is_parent=True, type_info=Models.SECTION)
+    values = Field(is_child=True, optional=False, type=list, type_info=Models.VALUE)
+
+Models._MODEL_MAP[Models.PROPERTY] = PropertyModel
+
+
+class ValueModel(RestResult):
+    model = Field(type=str, default=Models.VALUE)
+    value = Field(type=str, optional=False)
+
+    parent = Field(is_parent=True, type_info=Models.PROPERTY)
+
+Models._MODEL_MAP[Models.VALUE] = ValueModel
+
+
+class BlockModel(RestResult):
+    model = Field(type=str, default=Models.BLOCK)
+    name = Field(type=str)
+    index = Field(type=int, default=0)
+    description = Field(type=str)
+
+    recordingchannelgroups = Field(is_child=True, type=list, type_info=Models.RECORDINGCHANNELGROUP)
+    segments = Field(is_child=True, type=list, type_info=Models.SEGMENT)
+
+Models._MODEL_MAP[Models.BLOCK] = BlockModel
+
+
 class SegmentModel(RestResult):
     """Model for segment"""
     model = Field(type=str, default=Models.SEGMENT)
     name = Field(type=str, default="")
     index = Field(default=0)
 
-    block = Field(is_parent=True, type=str, type_info=Models.BLOCK)
+    block = Field(is_parent=True, type_info=Models.BLOCK)
 
     analogsignals = Field(is_child=True, type=list, type_info=Models.ANALOGSIGNAL)
     #irregularlysampledsignals = Field(is_child=True, type=list, type_info=Models.IRREGULARLYSAMPLEDSIGNAL)
