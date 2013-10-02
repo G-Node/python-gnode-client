95177eb80733689fe714419fe9a0ab4d7af840b2
A. Stoewer <adrian.stoewer@rz.ifi.lmu.de>
2013-10-02 18:15:51 +0200

    Quick test: test for set method.

--- gnodeclient/test/quick.py
+++ gnodeclient/test/quick.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+import random
+import string
+
 from gnodeclient.model.rest_model import Models
 from gnodeclient import session
 
@@ -15,6 +18,7 @@ for b in blocks:
     print str(b)
 
 signals = s.select(Models.ANALOGSIGNAL)
+loc = signals[20].location
 sig = s.get(signals[20].location, refresh=True)
 seg = sig.segment
 bl = seg.block
@@ -22,3 +26,11 @@ bl = seg.block
 
 print str(sig.segment)
 print sig.segment.name
+
+
+rand_name = ''.join(random.choice(string.lowercase) for i in range(10))
+print "Random name: " + rand_name
+print "Segment name: " + seg.name
+seg.name = rand_name
+new_seg = s.set(seg)
+print "Segment new name: " + new_seg.name
