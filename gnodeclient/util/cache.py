b45e7e939668e1bbd96936eae81a800bb61a3bfd
A. Stoewer <adrian.stoewer@rz.ifi.lmu.de>
2013-10-08 17:37:52 +0200

    Cache: documentation

--- gnodeclient/util/cache.py
+++ gnodeclient/util/cache.py
@@ -21,10 +21,10 @@ class Cache(object):
         """
         Cache initialisation
 
-        :param location:
-        :type location:
-        :param base_dir:
-        :type base_dir:
+        :param location: The location of the base directory, by default a suitable location is chosen.
+        :type location: str
+        :param base_dir: The name of the base directory.
+        :type base_dir: str
         """
         if location is None:
             self.__base_dir = appdirs.user_cache_dir(base_dir)
@@ -47,14 +47,23 @@ class Cache(object):
 
     @property
     def base_dir(self):
+        """
+        The path to the base directory.
+        """
         return self.__base_dir
 
     @property
     def file_dir(self):
+        """
+        The path to the directory where cached files are stored.
+        """
         return self.__file_dir
 
     @property
     def obj_dir(self):
+        """
+        The path to the directory where cached objects are stored.
+        """
         return self.__obj_dir
 
     #
@@ -62,18 +71,41 @@ class Cache(object):
     #
 
     def set(self, location, data):
-        ident = urlparse.urlparse(location).path.split("/")[-1].lower()
-        f_name = self.obj_cache_path(ident)
+        """
+        Caches some object under a certain identifier. The identifier is extracted from the given
+        location.
 
-        all_data = self.secure_read(f_name, {})
+        Example locations:
+            "http://host/foo/identifier?param=val"
+            "/foo/identifier"
+            "identifier"
+
+        :param location: An url or path that ends with a unique identifier or the identifier itself.
+        :type location: str
+        :param data: Some object that can be serialized by pickle.
+        :type data: object
+        """
+        ident = urlparse.urlparse(location).path.split("/")[-1].lower()
+        f_name = self._obj_cache_path(ident)
+
+        all_data = self._secure_read(f_name, {})
         all_data[ident] = data
-        self.secure_write(f_name, all_data)
+        self._secure_write(f_name, all_data)
 
     def get(self, location):
-        ident = urlparse.urlparse(location).path.split("/")[-1].lower()
-        f_name = self.obj_cache_path(ident)
+        """
+        Get an object form the cache.
 
-        all_data = self.secure_read(f_name, {})
+        :param location: An url or path that ends with a unique identifier or the identifier itself.
+        :type location: str
+
+        :returns: The cached object or None if not found.
+        :rtype: object
+        """
+        ident = urlparse.urlparse(location).path.split("/")[-1].lower()
+        f_name = self._obj_cache_path(ident)
+
+        all_data = self._secure_read(f_name, {})
 
         if ident in all_data:
             return all_data[ident]
@@ -81,10 +113,19 @@ class Cache(object):
             return None
 
     def delete(self, location):
-        ident = urlparse.urlparse(location).path.split("/")[-1].lower()
-        f_name = self.obj_cache_path(ident)
+        """
+        Delete an object form the cache.
 
-        all_data = self.secure_read(f_name, {})
+        :param location: An url or path that ends with a unique identifier or the identifier itself.
+        :type location: str
+
+        :returns: True if the object was deleted, False if not found.
+        :rtype: bool
+        """
+        ident = urlparse.urlparse(location).path.split("/")[-1].lower()
+        f_name = self._obj_cache_path(ident)
+
+        all_data = self._secure_read(f_name, {})
 
         if ident in all_data:
             del all_data[ident]
@@ -93,24 +134,50 @@ class Cache(object):
             return False
 
     def set_file(self, location, data):
-        ident = urlparse.urlparse(location).path.split("/")[-1].lower()
-        f_name = self.file_cache_path(ident)
+        """
+        Write file data to the cache.
 
-        self.secure_write(f_name, data, False)
+        :param location: An url or path that ends with a unique identifier or the identifier itself.
+        :type location: str
+        :param data: A byte-string that will be stored in a file.
+        :type data: str
+        """
+        ident = urlparse.urlparse(location).path.split("/")[-1].lower()
+        f_name = self._file_cache_path(ident)
+
+        self._secure_write(f_name, data, False)
 
     def get_file(self, location):
+        """
+        Get file data form the cache.
+
+        :param location: An url or path that ends with a unique identifier or the identifier itself.
+        :type location: str
+
+        :returns: The cached file data or None if not found.
+        :rtype: str
+        """
         ident = urlparse.urlparse(location).path.split("/")[-1].lower()
-        f_name = self.file_cache_path(ident)
+        f_name = self._file_cache_path(ident)
 
         if path.isfile(f_name):
-            data = self.secure_read(f_name, serialize=False)
+            data = self._secure_read(f_name, serialize=False)
             return data
         else:
             return None
 
     def delete_file(self, location):
+        """
+        Delete file data form the cache.
+
+        :param location: An url or path that ends with a unique identifier or the identifier itself.
+        :type location: str
+
+        :returns: True if the file was deleted, False if not found.
+        :rtype: bool
+        """
         ident = urlparse.urlparse(location).path.split("/")[-1].lower()
-        f_name = self.file_cache_path(ident)
+        f_name = self._file_cache_path(ident)
 
         if path.isfile(f_name):
             os.remove(f_name)
@@ -119,6 +186,9 @@ class Cache(object):
             return False
 
     def clear(self):
+        """
+        Erase all data from the cache.
+        """
         dirs = (self.base_dir, self.file_dir, self.obj_dir)
         for d in dirs:
             if path.exists(d):
@@ -129,14 +199,14 @@ class Cache(object):
     # Helper methods
     #
 
-    def obj_cache_path(self, ident):
+    def _obj_cache_path(self, ident):
         prefix = ident[0:2]
         return path.join(self.obj_dir, prefix)
 
-    def file_cache_path(self, ident):
+    def _file_cache_path(self, ident):
         return path.join(self.file_dir, ident)
 
-    def secure_read(self, f_name, default=None, serialize=True):
+    def _secure_read(self, f_name, default=None, serialize=True):
         f_handle = None
         f_name_tmp = path.join(self.base_dir, "temp_" + "".join(random.choice(string.lowercase) for _ in range(20)))
 
@@ -173,7 +243,7 @@ class Cache(object):
 
         return data
 
-    def secure_write(self, f_name, data, serialize=True):
+    def _secure_write(self, f_name, data, serialize=True):
         f_handle = None
         f_name_tmp = path.join(self.base_dir, "temp_" + "".join(random.choice(string.lowercase) for _ in range(20)))
 
