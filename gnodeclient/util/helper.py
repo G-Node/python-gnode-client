6d0406a52a6d08980f972f3bcea21d3cb22e1e83
A. Stoewer <adrian.stoewer@rz.ifi.lmu.de>
2013-10-11 12:57:06 +0200

    Helper: added module for small helper functions

---
+++ gnodeclient/util/helper.py
@@ -0,0 +1,24 @@
+"""
+Miscellaneous helper functions.
+"""
+
+import string
+import random
+
+try:
+    import urlparse
+except ImportError:
+    # python > 3.1 has not module urlparse
+    import urllib.parse as urlparse
+
+
+def id_from_location(location):
+    ident = urlparse.urlparse(location).path.strip("/").split("/")[-1].lower()
+    return ident
+
+
+def random_str(length=20, prefix="", separator="_"):
+    rnd = "".join(random.choice(string.lowercase) for _ in range(length))
+    if prefix is not None and len(prefix) > 0:
+        rnd = prefix + separator + rnd
+    return rnd
