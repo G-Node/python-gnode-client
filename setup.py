7a2d15366e7c59e21f77a24a01129dd983dfb52e
A. Stoewer <adrian.stoewer@rz.ifi.lmu.de>
2013-10-31 14:15:03 +0100

    setup.py

--- setup.py
+++ setup.py
@@ -3,21 +3,19 @@ try:
 except ImportError:
     from distutils.core import setup
 
-from gnodeclient import GNODECLIENT_VERSION
-
-with open('README.rst') as file:
+with open("README.rst") as file:
     long_description = file.read()
 
-packages = (
-    'gnodeclient',
-    'gnodeclient.model',
-    'gnodeclient.store',
-    'gnodeclient.test',
-    'gnodeclient.util',
-    'gnodeclient.result'
-)
+packages = [
+    "gnodeclient",
+    "gnodeclient.model",
+    "gnodeclient.store",
+    "gnodeclient.test",
+    "gnodeclient.util",
+    "gnodeclient.result"
+]
 
-requires = (
+requires = [
     "requests >= 0.12.0",
     "appdirs >= 1.2.0",
     "quantities >= 0.10.0",
@@ -25,32 +23,31 @@ requires = (
     "requests-futures >= 0.9.0",
     "odml >= 1.0",
     "h5py >= 2.0.1"
-)
+]
 
 setup(
-    name='gnodeclient',
-    version=GNODECLIENT_VERSION,
-    author='A. Stoewer, A. Sobolev',
-    author_email='adrian.stoewer@rz.ifi.lmu.de',
+    name="gnodeclient",
+    version="0.2.0",
+    author="A. Stoewer, A. Sobolev",
+    author_email="adrian.stoewer@rz.ifi.lmu.de",
     packages=packages,
-    package_dir={'gnodeclient': 'gnodeclient'},
-    package_data={'gnodeclient': ['LICENSE.txt', 'README.rst']},
+    package_dir={"gnodeclient": "gnodeclient"},
+    package_data={"gnodeclient": ["LICENSE.txt", "README.rst"]},
     test_suite="gnodeclient.test.test_all",
     scripts=[],
-    url='https://github.com/G-Node/python-gnode-client',
-    license='LGPL',
-    description='Client for the G-Node REST API.',
+    url="https://github.com/G-Node/python-gnode-client",
+    license="LGPL",
+    description="Client for the G-Node REST API.",
     include_package_data=True,
     long_description=long_description,
     install_requires=requires,
-    classifiers=(
-        'Development Status :: 4 - Beta',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.1',
-        'Programming Language :: Python :: 3.2',
-
-    ),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.1",
+        "Programming Language :: Python :: 3.2"
+    ]
 )
