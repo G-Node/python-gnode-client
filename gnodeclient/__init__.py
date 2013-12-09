9e943b3d6747ec350d511b00036045b174733042
Andrey <sobolev.andrey@gmail.com>
2013-12-09 13:17:23 +0100

    added native neo and odml classes to the __all__ of the core module

--- gnodeclient/__init__.py
+++ gnodeclient/__init__.py
@@ -13,5 +13,7 @@ This package contains the client implementation for the G-Node REST API.
 
 from gnodeclient.session import Session, close, create, GNODECLIENT_VERSION, GNODECLIENT_RELEASE
 from gnodeclient.model.models import Model
+from gnodeclient.result import Native
 
-__all__ = ("session", "model", "store", "conf", "test", "Model", "GNODECLIENT_VERSION", "GNODECLIENT_RELEASE")
+__all__ = ("session", "model", "store", "conf", "test", "Native",
+           "Model", "GNODECLIENT_VERSION", "GNODECLIENT_RELEASE")
