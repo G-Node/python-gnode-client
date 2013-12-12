3f5f91357ad86eada52ae67549400f7aa7542fd8
A. Stoewer <adrian.stoewer@rz.ifi.lmu.de>
2013-12-12 09:21:07 +0100

    Stub for tools module.

---
+++ gnodeclient/tools.py
@@ -0,0 +1,25 @@
+# Python G-Node Client
+#
+# Copyright (C) 2013  A. Stoewer
+#                     A. Sobolev
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License (see LICENSE.txt).
+
+"""
+This module defines some utility functions that provide implementations for
+some common procedures.
+"""
+
+from __future__ import print_function, absolute_import, division
+
+from gnodeclient import Session
+
+
+def upload_odml_tree(session, section):
+    pass
+
+
+def upload_neo_structure(session, neo):
+    pass
